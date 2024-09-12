7730c976db1fc8265d7abbecef641973077aba47
hayaboy <hayaboy@korea.ac.kr>
2024-09-12 18:58:56 +0900

    dsv

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
+
 def insert_db():
     # 엑셀 파일 경로
     company_xlsx_file_path = (
@@ -8,36 +9,30 @@ def insert_db():
     )
     df_company = pd.read_excel(company_xlsx_file_path, engine="openpyxl")
 
-
     year2019_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/2019.xlsx"
     )
     df_year2019 = pd.read_excel(year2019_xlsx_file_path, engine="openpyxl")
-
 
     year2020_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/2020.xlsx"
     )
     df_year2020 = pd.read_excel(year2020_xlsx_file_path, engine="openpyxl")
 
-
     year2021_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/2021.xlsx"
     )
     df_year2021 = pd.read_excel(year2021_xlsx_file_path, engine="openpyxl")
-
 
     year2022_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/2022.xlsx"
     )
     df_year2022 = pd.read_excel(year2022_xlsx_file_path, engine="openpyxl")
 
-
     year2023_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/2023.xlsx"
     )
     df_year2023 = pd.read_excel(year2023_xlsx_file_path, engine="openpyxl")
-
 
     faq_data_xlsx_file_path = (
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/faq.xlsx"
@@ -48,7 +43,6 @@ def insert_db():
         r"C:/SKN02-1st-4Team-main/SKN02-1st-4Team-main/data/car_total.xlsx"
     )
     df_car = pd.read_excel(car_total_xlsx_file_path, engine="openpyxl")
-
 
     # 필요 없는 'Unnamed: 0' 열 제거
     if "Unnamed: 0" in df_company.columns:
@@ -68,21 +62,19 @@ def insert_db():
 
     if "Unnamed: 0" in df_year2023.columns:
         df_year2023 = df_year2023.drop(columns=["Unnamed: 0"])
-    
+
     if "Unnamed: 0" in df_faq.columns:
         df_faq = df_faq.drop(columns=["Unnamed: 0"])
 
     if "Unnamed: 0" in df_car.columns:
         df_car = df_car.drop(columns=["Unnamed: 0"])
 
-
     # MySQL connection 세팅
     db_user = "root"
-    db_password = "12341234"
-    db_host = "localhost"
+    db_password = "1111"
+    db_host = "125.132.237.54"
     db_port = "3306"
     db_name = "project1db"
-
 
     # SQLAlchemy 엔진 생성 (데이터베이스 생성 전용)
     create_engine_without_db = create_engine(
@@ -97,11 +89,10 @@ def insert_db():
     engine = create_engine(
         f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     )
-    
+
     try:
         with engine.connect() as conn:
             conn.execute(text(f"USE project1db;"))
-
 
             # 중복되지 않도록 테이블 DROP하기
             conn.execute((text)(f"DROP TABLE IF EXISTS year2019;"))
@@ -112,7 +103,6 @@ def insert_db():
             conn.execute((text)(f"DROP TABLE IF EXISTS faq;"))
             conn.execute((text)(f"DROP TABLE IF EXISTS company;"))
             conn.execute((text)(f"DROP TABLE IF EXISTS car;"))
-
 
             create_company_table = """
             CREATE TABLE IF NOT EXISTS company(
@@ -195,20 +185,29 @@ def insert_db():
             conn.execute(text(create_faq_table))
             conn.execute(text(create_car_table))
 
-
             # Dataframe.to_sql을 사용해서 데이터 넣기
-            df_company.to_sql('company', con=engine, index=False, if_exists="append", method="multi")
-            df_year2019.to_sql('year2019', con=engine, index=False, if_exists="append", method="multi")
-            df_year2020.to_sql('year2020', con=engine, index=False, if_exists="append", method="multi")
-            df_year2021.to_sql('year2021', con=engine, index=False, if_exists="append", method="multi")
-            df_year2022.to_sql('year2022', con=engine, index=False, if_exists="append", method="multi")
-            df_year2023.to_sql('year2023', con=engine, index=False, if_exists="append", method="multi")
-            df_faq.to_sql('faq', con=engine, index=False, if_exists="append", method="multi")
-            df_car.to_sql('car', con=engine, index=False, if_exists="append", method="multi")
-    
+            df_company.to_sql('company', con=engine, index=False,
+                              if_exists="append", method="multi")
+            df_year2019.to_sql('year2019', con=engine,
+                               index=False, if_exists="append", method="multi")
+            df_year2020.to_sql('year2020', con=engine,
+                               index=False, if_exists="append", method="multi")
+            df_year2021.to_sql('year2021', con=engine,
+                               index=False, if_exists="append", method="multi")
+            df_year2022.to_sql('year2022', con=engine,
+                               index=False, if_exists="append", method="multi")
+            df_year2023.to_sql('year2023', con=engine,
+                               index=False, if_exists="append", method="multi")
+            df_faq.to_sql('faq', con=engine, index=False,
+                          if_exists="append", method="multi")
+            df_car.to_sql('car', con=engine, index=False,
+                          if_exists="append", method="multi")
+
     except Exception as e:
         print(f"Error: {e}")
 
     # Connection 닫기
     engine.dispose()
+
+
 insert_db()
